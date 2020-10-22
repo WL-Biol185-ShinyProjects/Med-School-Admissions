8b409cb40025051286f5346dca45e79ece21b0d5
Grace Amaden <amadeng21@mail.wlu.edu>
2020-10-22 12:51:14 -0400

    UI update and file name change

--- UI.R
+++ UI.R
@@ -1,41 +1,55 @@
 library(shiny)
 
 
-navbarPage("Medical School Admissions",
+navbarPage("Medical School Admissions")
+
           tabPanel("About")
-)
           
            tabPanel("Academics")
            fluidPage(
             sidebarLayout(
               sidebarPanel(
-                numericInput("plotType", "Enter GPA",
-                             c("Scatter"="p"),
-                numericInput("plotType", "Enter MCAT score",
-                             c("Scatter"="p"),
-               selectInput("plotType", "Select schools",
-                                          c("Scatter"="p")
-                          )
-                            )
-                            )
+                numericInput(
+                             inputId = "GPA_Input",
+                             label = "Enter GPA",
+                             value = "0",
+                             min = "0",
+                             max = "4.0"
+                             ),
+                            
+                numericInput(
+                            inputId = "MCAT_Input",
+                            label = "Enter MCAT",
+                            value = "472",
+                            min = "472",
+                            max = "528"
+                            ),
+                            
+               selectInput(
+                          inputId = "Schools_Input",
+                          label = "Select Schools",
+                          choices = list("School_Scores_Data$'Medical School'"),
+                          multiple = TRUE
                           ),
-                       mainPanel()
-                          )
-                    )
-          
-            
+                 
+                       ),
+              mainPanel()
+                     )
+                   )
+           
   
         tabPanel("By state")
         fluidPage(
           sidebarLayout(
             sidebarPanel(
-                selectInput("plotType", "choose your state",
-                              c("Scatter"="p")
-                           )
-                        ),
+                selectInput(
+                            inputId = "State_Input",
+                            label = "Select Your State",
+                            choices = list("$''"),
+                            multiple = TRUE
+                ),
                       mainPanel()
-                       )
-                 )
+            
         
                  tabPanel("By Major")
                   
