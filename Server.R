7687626396ade94e8713616fee6ae29df4adbeab
halfant99 <halfant99@gmail.com>
2020-10-29 12:49:52 -0400

    fix server

--- Server.R
+++ Server.R
@@ -8,7 +8,9 @@ output$'Academic_Plot' <- renderPlot({Academics_Data %>%
                           filter(MCAT_Input==input$MCAT_Input)%>%
                           ggplot(aes_string("GPA_Input" , "MCAT_Input", fill = "Schools_Input"))
                                 +geom_point()
-                                 
+  
+
+
                                     })
   
 
