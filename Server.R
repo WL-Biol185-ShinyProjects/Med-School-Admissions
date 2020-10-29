69fad4be0606856520bd6d8c233fddb6c263003a
halfant99 <halfant99@gmail.com>
2020-10-29 12:10:05 -0400

    server repair

--- Server.R
+++ Server.R
@@ -5,7 +5,7 @@ function(input, output)
 {
 output$'Academic_Plot' <- renderPlot({GPA_Input %>% 
                           filter(GPA_Input==input$GPA_Input)%>%
-                          ggplot(Academics_Data, aes(GPA , MCAT))
+                          ggplot(aes(GPA , MCAT))
                               +geom_point()
                                  
                                     })
