8fa0d312e6d4e5854b99532731d1d9d5cc57623b
Grace Amaden <amadeng21@mail.wlu.edu>
2020-11-17 15:23:33 -0500

    values for target school list

--- ui.r
+++ ui.r
@@ -128,7 +128,7 @@ navbarPage("Medical School Admissions",
                       numericInput(
                                   inputId = "GPA_Input_2",
                                   label = "Enter GPA",
-                                  value = "0",
+                                  value = "3.8",
                                   min = "0",
                                   max = "4.0"
                                   ),
@@ -136,7 +136,7 @@ navbarPage("Medical School Admissions",
                      numericInput(
                                  inputId = "MCAT_Input_2",
                                  label = "Enter MCAT",
-                                 value = "472",
+                                 value = "510",
                                  min = "472",
                                  max = "528"
                                  )
